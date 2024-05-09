057f9ff39eebf4e1304a581c30b55044aa9abd14
Mark <mrkcarlos576@gmail.com>
2024-05-09 15:49:42 +0300

    assignment complete

--- src/products.js
+++ src/products.js
@@ -1,59 +1,66 @@
-import product1 from "./assets/products/1.png";
-import product2 from "./assets/products/2.png";
-import product3 from "./assets/products/3.png";
-import product4 from "./assets/products/4.png";
-import product5 from "./assets/products/5.png";
-import product6 from "./assets/products/6.webp";
-import product7 from "./assets/products/7.webp";
-import product8 from "./assets/products/8.webp";
+import tee from "./assets/tee.jpeg";
+import frank from "./assets/frank.jpeg";
+import hat from "./assets/hat.jpeg";
+import hoodie from "./assets/hoodie.jpeg";
+import ice from "./assets/ice.jpeg";
+import jordan from "./assets/Jordan.jpeg";
+import vancleef from "./assets/vancleef.jpeg";
+import vlone from "./assets/vlone.jpeg";
+import north from "./assets/north.jpeg"
 
 export const PRODUCTS = [
   {
     id: 1,
-    productName: "IPhone",
-    price: 999.0,
-    productImage: product1,
+    productName: "The North Face puffer jacket",
+    price: 6000,
+    productImage: north,
+  },
+  {
+    id: 1,
+    productName: "Frank Ocean Graphic Tee",
+    price: 3999,
+    productImage: frank,
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
-    price: 1999.0,
-    productImage: product2,
+    productName: "Chrome Hearts Tee",
+    price: 4500,
+    productImage: tee,
   },
   {
     id: 3,
-    productName: "Cannon M50 Camera",
-    price: 699.0,
-    productImage: product3,
+    productName: "LA hat",
+    price: 1550,
+    productImage: hat,
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
-    price: 228.0,
-    productImage: product4,
+    productName: "Chrome Hearts Hoodie",
+    price: 7250,
+    productImage: hoodie,
   },
   {
     id: 5,
-    productName: "LED Light Strips",
-    price: 19.99,
-    productImage: product5,
+    productName: "Ice Spice tee",
+    price: 3000,
+    productImage: ice,
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
-    price: 68.0,
-    productImage: product6,
+    productName: "Jordan 1's",
+    price: 9000,
+    productImage: jordan,
   },
   {
     id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
-    price: 120.0,
-    productImage: product7,
+    productName: "Van Cleef Bracelet",
+    price: 2999,
+    productImage: vancleef,
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
-    price: 40.0,
-    productImage: product8,
+    productName: "Vlone Tee",
+    price: 6000,
+    productImage: vlone,
   },
 ];
